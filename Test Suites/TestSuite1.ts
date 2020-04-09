071ca5a6afa367a02e451167aa43fb06a1845e47
ANUPAMA DHIR <sabhssabhs@yahoo.com>
2020-04-09 10:16:42 +0800

    test message

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1281c36d-a54e-4c68-b116-127e668c2cf9</testSuiteGuid>
+</TestSuiteEntity>
